5f884ad485d4241c25bc47809c10be56655a1551
amirkiyanvash <amirkiyanvash20@gmail.com>
2022-02-18 11:16:41 +0330

    signup style sheet added to project

--- registr-form/src/components/SignUp.js
+++ registr-form/src/components/SignUp.js
@@ -3,6 +3,7 @@ import {valiDate} from "./validate";
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { notify } from './toast';
+import styles from "./SignUp.module.css"
 
 const SignUp = () => {
     const [data , setData]=useState({
@@ -58,39 +59,71 @@ const SignUp = () => {
 
 
     return (
-        <div>
-            <form onSubmit={submitHandler}>
-                <h2>Sign Up</h2>
-                <div>
+        <div className={styles.container}>
+            <form className={styles.formContainer} onSubmit={submitHandler}>
+                <h2 className={styles.header}>Sign Up</h2>
+                <div className={styles.formField}>
                     <label>Name</label>
-                    <input type="text" name='name' value={data.name} onChange={changHandler} onFocus={tochHandler} />
+                    <input 
+                    className={(errors.name && touched.name) ? styles.uncompleted : styles.formInput}
+                    type="text" 
+                    name='name' 
+                    value={data.name} 
+                    onChange={changHandler} 
+                    onFocus={tochHandler} />
                     {errors.name && touched.name &&  <span>{errors.name}</span>}
-                <div>
-                <div>
+                </div>
+                <div className={styles.formField}>
                     <label>Email</label>
-                    <input type="email" name='email' value={data.email} onChange={changHandler} onFocus={tochHandler} />
+                    <input
+                    className={(errors.email && touched.email) ? styles.uncompleted : styles.formInput} 
+                    type="email" 
+                    name='email' 
+                    value={data.email} 
+                    onChange={changHandler} 
+                    onFocus={tochHandler} />
                     {errors.email && touched.email &&  <span>{errors.email}</span>}
                 </div>
+                <div className={styles.formField}>
                     <label>Password</label>
-                    <input type="password" name='password' value={data.password} onChange={changHandler} onFocus={tochHandler} />
+                    <input
+                    className={(errors.password && touched.password) ? styles.uncompleted : styles.formInput} 
+                    type="password" 
+                    name='password' 
+                    value={data.password} 
+                    onChange={changHandler} 
+                    onFocus={tochHandler} />
                     {errors.password && touched.password && <span>{errors.password}</span>}
                 </div>
-                <div>
+                <div className={styles.formField}>
                     <label>Confirm Password</label>
-                    <input type="password" name='confirmPassword' value={data.confirmPassword} onChange={changHandler} onFocus={tochHandler} />
+                    <input
+                    className={(errors.confirmpassword && touched.confirmPassword) ? styles.uncompleted : styles.formInput} 
+                    type="password" 
+                    name='confirmPassword' 
+                    value={data.confirmPassword} 
+                    onChange={changHandler} 
+                    onFocus={tochHandler} />
                     {errors.confirmpassword  && touched.confirmPassword &&   <span>{errors.confirmpassword}</span>}
                 </div>
                
-                <div>
+                <div className={styles.formField}>
+                    <div className={styles.checkboxContainer}>
                     <label> I accet terms of privacy policy</label>
-                    <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange={changHandler} onFocus={tochHandler} />
+                    <input 
+                    type="checkbox" 
+                    name='isAccepted' 
+                    value={data.isAccepted} 
+                    onChange={changHandler} 
+                    onFocus={tochHandler} />
+                    </div>
                     {errors.isAccepted && touched.isAccepted &&  <span>{errors.isAccepted}</span>}
                 </div>
-                <div>
+                <div className={styles.formButtons}>
                     <a href="#">Login</a>
                     <button type='submit'>Sign up</button>
                 </div>
-                </div>
+                
             </form>
             <ToastContainer />
         </div>
